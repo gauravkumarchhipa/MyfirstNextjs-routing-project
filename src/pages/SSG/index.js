21e837d51b4d4d5cd900c450d0a3211b4e7530ca
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-05-14 21:25:09 +0530

    fallback

--- src/pages/SSG/index.js
+++ src/pages/SSG/index.js
@@ -6,6 +6,15 @@ function index() {
             <Link legacyBehavior href={'/SSG/post'}>
                 <h1>post </h1>
             </Link>
+            <Link legacyBehavior href={'/SSG/allpost'}>
+                <h1>All Post </h1>
+            </Link>
+            <Link legacyBehavior href={'/SSG/fallbacktrue'}>
+                <h1>Fallback true </h1>
+            </Link>
+            <Link legacyBehavior href={'/SSG/fallbackblocking'}>
+                <h1>Fallback blocking </h1>
+            </Link>
         </div>
     );
 }
